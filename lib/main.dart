7c9df19f52ea574cb52d36974810b58ce8a695ca
Worakorn <manager.im@gmail.com>
2019-03-29 10:50:48 +0700

    Diving Deeper Into the Syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(MyApp());
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
-        )
+        ),
       ),
     );
   }
